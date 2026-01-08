da3e683a38545eac5b9d513dc8b6086d08726898
Riccardo <richi-97@live.it>
2026-01-08 14:02:46 +0100

    Update Es1.java

--- src/es1/Es1.java
+++ src/es1/Es1.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Es1 {
     public static void main(String[] args) {
-        Es1 es1 = new Es1();
         Scanner scanner = new Scanner(System.in);
         System.out.println("inserisci una parola");
         String parola = scanner.nextLine();
-        System.out.println(es1.stringaPari(parola));
+        System.out.println(stringaPari(parola));
+        System.out.println("inserisci un anno per capire se è bisestile");
         int anno = Integer.parseInt(scanner.nextLine());
-        System.out.println(es1.annoBisestile(anno));
+        System.out.println(annoBisestile(anno));
 
     }
 
-    public boolean stringaPari(String parola) {
+    public static boolean stringaPari(String parola) {
         if(parola.length() % 2 == 0) return true;
         else return false;
     }
-    public boolean annoBisestile(int anno) {
+    public static boolean annoBisestile(int anno) {
         if(anno % 4 == 0) {
             if(anno % 100 == 0 ) {
                 if(anno % 400 == 0) {
